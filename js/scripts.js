d821a898434d26549aef30b3d23b21c9c11eaa12
Sarah Redway <sarah.redway@shields.energy>
2019-10-29 11:58:05 +0000

    initial styling

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    id: 1,
+    text: 'Question 1',
+    answerOptions: ['Answer1', 'Answer2', 'Answer3'],
+    correctAnswer: 'Answer 2'
+  },
+  {
+    id: 2,
+    text: 'Question 2',
+    answerOptions: ['Answer1', 'Answer2', 'Answer3'],
+    correctAnswer: 'Answer 3'
+  },
+  {
+    id: 3,
+    text: 'Question 3',
+    answerOptions: ['Answer1', 'Answer2', 'Answer3'],
+    correctAnswer: 'Answer 1'
+  }
+];
+
+selectedQuestionIndex = 0;
+
+totalScore = 0;
